9d25e1414d8c9743bf1681b2860a4f04bf63df08
tarun-nagpal <tarunnagpal81@gmail.com>
2013-10-11 20:21:37 +0530

    check for header params

--- socialauth/src/main/java/org/brickred/socialauth/oauthstrategy/OAuth2.java
+++ socialauth/src/main/java/org/brickred/socialauth/oauthstrategy/OAuth2.java
@@ -310,7 +310,8 @@ public class OAuth2 implements OAuthStrategyBase {
 				|| MethodType.PUT.toString().equals(methodType)) {
 			if (sb.length() > 0) {
 				if (bodyStr != null) {
-					if (headerParams.containsKey("Content-Type")) {
+					if (headerParams != null
+							&& headerParams.containsKey("Content-Type")) {
 						String val = headerParams.get("Content-Type");
 						if (!"application/json".equals(val)) {
 							bodyStr += "&";
