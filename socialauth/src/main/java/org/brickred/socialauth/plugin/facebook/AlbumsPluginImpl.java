5539232770d6b9adfaeb6e358a266d301039f32b
Ben Zhong <benzhong@Bens-MacBook-Pro-15.local>
2017-02-22 13:44:35 +1300

    Revert "Fix Facebook photo issue."
    This reverts commit f61d3b01c620aa2c3f180420051fded851e0be10.

--- socialauth/src/main/java/org/brickred/socialauth/plugin/facebook/AlbumsPluginImpl.java
+++ socialauth/src/main/java/org/brickred/socialauth/plugin/facebook/AlbumsPluginImpl.java
@@ -50,7 +50,7 @@ public class AlbumsPluginImpl implements AlbumsPlugin, Serializable {
 
 	private static final long serialVersionUID = 5350785649768508189L;
 	private static final String ALBUMS_URL = "https://graph.facebook.com/v2.2/me/albums";
-	private static final String ALBUM_PHOTOS_URL = "https://graph.facebook.com/v2.2/%1$s/photos?fields=names,link,picture,images";
+	private static final String ALBUM_PHOTOS_URL = "https://graph.facebook.com/v2.2/%1$s/photos";
 	private static final String ALBUM_COVER_URL = "https://graph.facebook.com/v2.2/%1$s/picture?access_token=%2$s";
 	private final Log LOG = LogFactory.getLog(this.getClass());
 
